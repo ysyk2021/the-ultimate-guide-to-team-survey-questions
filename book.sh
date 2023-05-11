9f602dc6d58c3bbb6e4c944a44babd29aa7583f6
ysykzheng <1020680508@qq.com>
2023-05-11 17:13:19 +0800

    page size

--- book.sh
+++ book.sh
@@ -1,7 +1,7 @@
 npm install
 npx honkit epub ./ the-ultimate-guide-to-team-survey-questions.epub
 
-ebook-convert the-ultimate-guide-to-team-survey-questions.epub the-ultimate-guide-to-team-survey-questions.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+ebook-convert the-ultimate-guide-to-team-survey-questions.epub the-ultimate-guide-to-team-survey-questions.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a4 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
 
 # brew install pdftk-java
 java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" the-ultimate-guide-to-team-survey-questions.pdf cat 2-end output the-ultimate-guide-to-team-survey-questions-FINAL.pdf
